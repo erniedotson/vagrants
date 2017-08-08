e0a4e136de0bedd5ef7cee0f0615a56e8fc52ea1
Ernest Dotson <edotson@gmail.com>
2017-08-08 12:24:51 -0400

    Add fix for VSCode/GitBash dir display colors in Ubuntu

--- scripts/provision_ubuntu-trusty64.sh
+++ scripts/provision_ubuntu-trusty64.sh
@@ -3,4 +3,7 @@ set -ex
 
 sudo apt-get update
 
+# Fix for Visual Studio Code / Git Bash Display
+grep -q -F 'LS_COLORS' /home/vagrant/.bashrc || echo 'export LS_COLORS="ow=01;36;40"' >> /home/vagrant/.bashrc
+
 echo "Provision script has completed!"
--- scripts/provision_ubuntu-xenial64.sh
+++ scripts/provision_ubuntu-xenial64.sh
@@ -3,4 +3,7 @@ set -ex
 
 sudo apt-get update
 
+# Fix for Visual Studio Code / Git Bash Display
+grep -q -F 'LS_COLORS' /home/ubuntu/.bashrc || echo 'export LS_COLORS="ow=01;36;40"' >> /home/ubuntu/.bashrc
+
 echo "Provision script has completed!"
